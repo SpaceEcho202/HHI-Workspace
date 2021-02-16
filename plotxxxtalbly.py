293400e1608b1c7330d35e5106e4a3b938e8bc66
Max Thinkpad <klemm.maximilian@outlook.de>
2021-02-16 19:02:33 +0100

    Much faster plot function

--- plotxxxtalbly.py
+++ plotxxxtalbly.py
@@ -7,16 +7,8 @@ import math
 import scipy.special
 from time import process_time as clock
 
-def track_time (snr_res__var, scpy):
-    #Call exception class
-    class ValueTooSmallError(Exception):
-        pass
-
-    class ValueNegativeError(Exception):
-        pass
-
-    #Call class which is responsible for creating the curve and all required default settings
-    class PlotTable:
+class PlotTable:
+        #Method which creates BLER vector depending on the previous specified Snr & nr_table -> "CQI, MCS"       
         def __init__(self, dictionary):
             self.grid          = dictionary.get('grid'            , "True"  )
             self.gridMinor     = dictionary.get('gridMinor'       , "True"  )
@@ -38,14 +30,15 @@ def track_time (snr_res__var, scpy):
                 self.ylabel    = dictionary.get('ylabel'          , str.upper(self.plotType))
             elif self.plotType == "Efficiency":
 
-        #ylim is determind by the max&min value in Y-vector if not predefined
+                #ylim is determind by the max&min value in Y-vector if not predefined
                 self.ylim      = dictionary.get('ylim', (np.min((self.CreateCurveDataEfficiency
-                                                        (self.plotTable, self.SnrMin, self.SnrMax, self.SnrResolution))[1]), 
-                                                        (np.max((self.CreateCurveDataEfficiency(self.plotTable, self.SnrMin, self.SnrMax, self.SnrResolution))[1]))))
+                                                        (self.plotTable, self.SnrMin, self.SnrMax,
+                                                         self.SnrResolution))[1]), 
+                                                        (np.max((self.CreateCurveDataEfficiency(self.plotTable,
+                                                         self.SnrMin, self.SnrMax, self.SnrResolution))[1]))))
                 self.yscale    = dictionary.get('yscale'          , 'linear')
                 self.ylabel    = dictionary.get('ylabel'          , "Spectral " + self.plotType + " in [bit/s/Hz]")
 
-        #Method that generates the curve and the associated plot
         def PlotCurve(self):
             plt.xlim(self.xlim)
             plt.ylim(self.ylim)
@@ -81,7 +74,7 @@ def track_time (snr_res__var, scpy):
                 for n in range(len(TempPlot[0])):
                     plt.plot(TempPlot[0][n],
                         TempPlot[1][n],
-                        label = str(self.label[0][n][1]) + " " + str(self.label[1][n][3]),
+                        label = str(self.label[0][n][1]) + " " +str(self.label[0][n][2]),
                         linestyle = self.lineSytle,
                         linewidth = self.linewidth ) 
 
@@ -110,7 +103,7 @@ def track_time (snr_res__var, scpy):
                 for n in range(len(TempPlot[0])):
                     plt.plot(TempPlot[0][n],
                         TempPlot[1][n],
-                        label = str(self.label[0][n][1]) + " " + str(self.label[1][n][3]),
+                        label = str(self.label[0][n][1]) + " " + str(self.label[0][n][2]) ,
                         linestyle = self.lineSytle,
                         linewidth = self.linewidth)
 
@@ -131,168 +124,78 @@ def track_time (snr_res__var, scpy):
                     fig.set_size_inches(17, 8)
                     plt.show()
 
-
-        #Method which creates BLER vector depending on the previous specified Snr & nr_table -> "CQI, MCS"       
         def CreateCurveDataBler(self, TableType, SnrMin, SnrMax, Resolution):
-            DataY     = []
-            TempDataY = []
 
             self.SnrVector = np.linspace(SnrMin, SnrMax, Resolution)
 
             if TableType == "CqiB":     
-                for n in range(len(self.SnrVector)):
-                    DataY.append(CalculationBlerEfficiency(
-                        SnrFactor=(Tables().getTableCqiB())[0][0],
-                        CodeRateFactor=(Tables().getTableCqiB())[0][1],
-                        MaximumRate=(Tables().getTableCqiB())[0][2]).GetBler(self.SnrVector[n]))
+               TempSnrFactor   = (Tables().getTableCqiB())[:, 0]
+               TempCodeRate    = (Tables().getTableCqiB())[:, 1]
+               TempMaximumRate = (Tables().getTableCqiB())[:, 2]
+               DataY           = (FastCalculationBlerEfficiency
+                                 (TempSnrFactor, TempCodeRate, TempMaximumRate)
+                                 .GetBler(self.SnrVector ))
 
-                for i in range(1, len(Tables().getTableCqiB())):
-                        for n in range(len(self.SnrVector)):
-                            TempDataY.append(CalculationBlerEfficiency(
-                                SnrFactor=(Tables().getTableCqiB())[i][0],
-                                CodeRateFactor=(Tables().getTableCqiB())[i][1],
-                                MaximumRate=(Tables().getTableCqiB())[i][2]).GetBler(self.SnrVector[n]))
+            if TableType == "McsA":     
+                TempSnrFactor   = (Tables().getTableMcsA())[:, 0]
+                TempCodeRate    = (Tables().getTableMcsA())[:, 1]
+                TempMaximumRate = (Tables().getTableMcsA())[:, 2]
+                DataY           = (FastCalculationBlerEfficiency
+                                 (TempSnrFactor, TempCodeRate, TempMaximumRate)
+                                 .GetBler(self.SnrVector ))
 
-                        DataY = np.row_stack((DataY, TempDataY))
-                        TempDataY = []
-
-            elif TableType == "McsA":
-                for n in range(len(self.SnrVector)):
-                            DataY.append(CalculationBlerEfficiency(
-                                SnrFactor=(Tables().getTableMcsA())[0][0],
-                                CodeRateFactor=(Tables().getTableMcsA())[0][1],
-                                MaximumRate=(Tables().getTableMcsA())[0][2]).GetBler(self.SnrVector[n]))
-
-                for i in range(1, len(Tables().getTableMcsA())):
-                        for n in range(len(self.SnrVector)):
-                            TempDataY.append(CalculationBlerEfficiency(
-                                SnrFactor=(Tables().getTableMcsA())[i][0],
-                                CodeRateFactor=(Tables().getTableMcsA())[i][1],
-                                MaximumRate=(Tables().getTableMcsA())[i][2]).GetBler(self.SnrVector[n]))
-
-                        DataY = np.row_stack((DataY, TempDataY))
-                        TempDataY = []
-
-            elif TableType == "McsB":
-                for n in range(len(self.SnrVector)):
-                            DataY.append(CalculationBlerEfficiency(
-                                SnrFactor=(Tables().getTableMcsB())[0][0],
-                                CodeRateFactor=(Tables().getTableMcsB())[0][1],
-                                MaximumRate=(Tables().getTableMcsB())[0][2]).GetBler(self.SnrVector[n]))
-
-                for i in range(1, len(Tables().getTableMcsB())):
-                        for n in range(len(self.SnrVector)):
-                            TempDataY.append(CalculationBlerEfficiency(
-                                SnrFactor=(Tables().getTableMcsB())[i][0],
-                                CodeRateFactor=(Tables().getTableMcsB())[i][1],
-                                MaximumRate=(Tables().getTableMcsB())[i][2]).GetBler(self.SnrVector[n]))
-
-                        DataY = np.row_stack((DataY, TempDataY))
-                        TempDataY = []
-
+            if TableType == "McsB":     
+                TempSnrFactor   = (Tables().getTableMcsB())[:, 0]
+                TempCodeRate    = (Tables().getTableMcsB())[:, 1]
+                TempMaximumRate = (Tables().getTableMcsB())[:, 2]
+                DataY           = (FastCalculationBlerEfficiency
+                                 (TempSnrFactor, TempCodeRate, TempMaximumRate)
+                                 .GetBler(self.SnrVector ))
             return [np.matlib.repmat(self.SnrVector, len(DataY), 1), DataY]
 
-
-        #Method which creates Efficiency Matrix depending on the previous specified Snr & nr_table -> "CQI, MCS" 
         def CreateCurveDataEfficiency(self, TableType, SnrMin, SnrMax, Resolution):
-            DataY     = []
-            TempDataY = []
+
             self.SnrVector = np.linspace(SnrMin, SnrMax, Resolution)
 
-            if TableType == "CqiB":
-                for n in range(len(self.SnrVector)):
-                            DataY.append(CalculationBlerEfficiency(
-                                SnrFactor=(Tables().getTableCqiB())[0][0],
-                                CodeRateFactor=(Tables().getTableCqiB())[0][1],
-                                MaximumRate=(Tables().getTableCqiB())[0][2]).GetEfficiency(self.SnrVector[n]))
+            if TableType == "CqiB":     
+                TempSnrFactor   = (Tables().getTableCqiB())[:, 0]
+                TempCodeRate    = (Tables().getTableCqiB())[:, 1]
+                TempMaximumRate = (Tables().getTableCqiB())[:, 2]
+                DataY           = (FastCalculationBlerEfficiency
+                                 (TempSnrFactor, TempCodeRate, TempMaximumRate)
+                                 .GetEfficiency(self.SnrVector ))
 
-                for i in range(1, len(Tables().getTableCqiB())):
-                        for n in range(len(self.SnrVector)):
-                            TempDataY.append(CalculationBlerEfficiency(
-                                SnrFactor=(Tables().getTableCqiB())[i][0],
-                                CodeRateFactor=(Tables().getTableCqiB())[i][1],
-                                MaximumRate=(Tables().getTableCqiB())[i][2]).GetEfficiency(self.SnrVector[n]))
-                        DataY = np.row_stack((DataY, TempDataY))
-                        TempDataY = []
+            if TableType == "McsA":     
+                TempSnrFactor   = (Tables().getTableMcsA())[:, 0]
+                TempCodeRate    = (Tables().getTableMcsA())[:, 1]
+                TempMaximumRate = (Tables().getTableMcsA())[:, 2]
+                DataY           = (FastCalculationBlerEfficiency
+                                 (TempSnrFactor, TempCodeRate, TempMaximumRate)
+                                 .GetEfficiency(self.SnrVector ))
 
-            elif TableType == "McsA":
-                for n in range(len(self.SnrVector)):
-                            DataY.append(CalculationBlerEfficiency(
-                                (Tables().getTableMcsA())[0][0],
-                                (Tables().getTableMcsA())[0][1],
-                                (Tables().getTableMcsA())[0][2]).GetEfficiency(self.SnrVector[n]))
-
-                for i in range(1, len(Tables().getTableMcsA())):
-                        for n in range(len(self.SnrVector)):
-                            TempDataY.append(CalculationBlerEfficiency(
-                                SnrFactor=(Tables().getTableMcsA())[i][0],
-                                CodeRateFactor=(Tables().getTableMcsA())[i][1],
-                                MaximumRate=(Tables().getTableMcsA())[i][2]).GetEfficiency(self.SnrVector[n]))
-                        DataY = np.row_stack((DataY, TempDataY))
-                        TempDataY = []
-
-            elif TableType == "McsB":
-                for n in range(len(self.SnrVector)):
-                            DataY.append(CalculationBlerEfficiency(
-                                SnrFactor=(Tables().getTableMcsB())[0][0],
-                                CodeRateFactor=(Tables().getTableMcsB())[0][1],
-                                MaximumRate=(Tables().getTableMcsB())[0][2]).GetEfficiency(self.SnrVector[n]))
-
-                for i in range(1, len(Tables().getTableMcsB())):
-                        for n in range(len(self.SnrVector)):
-                            TempDataY.append(CalculationBlerEfficiency(
-                                SnrFactor=(Tables().getTableMcsB())[i][0],
-                                CodeRateFactor=(Tables().getTableMcsB())[i][1],
-                                MaximumRate=(Tables().getTableMcsB())[i][2]).GetEfficiency(self.SnrVector[n]))
-
-                        DataY = np.row_stack((DataY, TempDataY))
-                        TempDataY = []
-
+            if TableType == "McsB":     
+                TempSnrFactor   = (Tables().getTableMcsB())[:, 0]
+                TempCodeRate    = (Tables().getTableMcsB())[:, 1]
+                TempMaximumRate = (Tables().getTableMcsB())[:, 2]
+                DataY           = (FastCalculationBlerEfficiency
+                                 (TempSnrFactor, TempCodeRate, TempMaximumRate)
+                                 .GetEfficiency(self.SnrVector ))
             return [np.matlib.repmat(self.SnrVector, len(DataY), 1), DataY]
-
-
-    #Method which creates the Bler || Efficiency vector            
-    class CalculationBlerEfficiency:
+class FastCalculationBlerEfficiency:
         def __init__(self, SnrFactor=1.0, CodeRateFactor=1.0, MaximumRate=1.0):
             self.SnrFactor      = SnrFactor
             self.CodeRateFactor = CodeRateFactor
             self.MaximumRate    = MaximumRate
-
-            #Exception call if the data does not meet the criteria description
-            try:
-                if self.CodeRateFactor < 0.0:
-                    raise ValueNegativeError
-                elif self.MaximumRate <= 0.0:
-                    raise ValueTooSmallError
-            except ValueNegativeError:
-                print("Code rate factor has to be positive!")
-            except ValueTooSmallError:
-                print("Maximum rate has to be larger than zero!")
-
+ 
         def GetBler(self, SnrInDecibel):
-            ScaleSnr = (SnrInDecibel-self.SnrFactor) / \
-                math.sqrt(2.0) / self.CodeRateFactor
-            if (scpy):
-                return (0.5 * (1 - math.erf(ScaleSnr)))
-            else:
-                return (0.5 * (1 - scipy.special.erf(ScaleSnr)))
+            ScaleSnr = [((SnrInDecibel)-self.SnrFactor[i])\
+                / math.sqrt(2.0) / self.CodeRateFactor[i] for i in range(len(self.SnrFactor))]
+            return [(0.5*(1-(scipy.special.erf(v)))) for v in ScaleSnr]
 
         def GetEfficiency(self, SnrInDecibel):
-            Bler = self.GetBler(SnrInDecibel)
-            return (1.0 - Bler) * self.MaximumRate
+            Bler = np.array(self.GetBler(SnrInDecibel))
+            return [((1.0 - Bler[i]) * self.MaximumRate[i]) for i in range(len(self.MaximumRate))]
 
-    #Calculates the time required to create the previously determined plot data
-    start_time = clock()
-
-    # Class call & method call to plot desired table "CqiB, McsA & McsB"
-    PlotTable({'PlotType': "Bler", 'PlotTable': "CqiB", 'SnrMax': 30, 'SnrMin': -10
-                ,'SnrResolution': snr_res__var}).PlotCurve()
-
-
-
-for i in [250]:
-    print("\nSnr-Resolution: " + str(i) + ":")
-    print("math.erf:")
-    track_time(i, scpy=False)
-    print("scipy.special.erf:")
-    track_time(i, scpy=True)
+start_time = clock()
+PlotTable({'PlotType': "Efficiency", 'PlotTable': "McsA", 'SnrMax': 20, 'SnrMin': -10
+                ,'SnrResolution': 10000}).PlotCurve()
